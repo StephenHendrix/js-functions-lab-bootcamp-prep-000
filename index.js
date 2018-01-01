8a88fa4ae6ef2d6aa9373a1a07dc68dec02d08cf
Stephen Hendrix <stephenth12@gmail.com>
2018-01-01 07:03:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-function happyHolidays() {return ("Happy holidays!")}
+function happyHolidays() {
+  return ("Happy holidays!")
+  }
+
 function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
+}
+
+function happyHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
 }
